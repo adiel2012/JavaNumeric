c93a423f23296a232e6579b33d017e4423b82e5e
adiel castano <adiel castano>
2017-03-11 15:59:34 -0800

    today Saturday

--- src/numericintegration/Montecarlo.java
+++ src/numericintegration/Montecarlo.java
@@ -14,38 +14,39 @@ import java.util.Random;
  * @author adiel
  */
 public class Montecarlo {
+
     public static void main(String[] args) {
         IFunction1Variable f = new IFunction1Variable() {
             @Override
             public double eval(double x) {
-                 return Math.cos(x);
+                return Math.cos(x);
             }
         };
-        
+
         double start = 0.5;
-        double end =  1; //Math.PI;
-        double result = doMonteCarlo(f,start, end);
-        
-        System.out.println("Result: "+result);
+        double end = 1; //Math.PI;
+        double result = doMonteCarlo(f, start, end);
+
+        System.out.println("Result: " + result);
     }
 
     private static double doMonteCarlo(IFunction1Variable f, double start, double end) {
         double mm = -1;
-        for (double i = start; i < end; i+= 0.001) {
+        for (double i = start; i < end; i += 0.001) {
             mm = Double.max(mm, f.eval(i));
         }
-        mm+=0.2;
+        mm += 0.2;
         int times = 1000000;
         int cont = 0;
         Random rnd = new Random();
         for (int i = 0; i < times; i++) {
-            double y = mm*rnd.nextDouble();
-            double x = start+ (end-start)*rnd.nextDouble();
-            if(f.eval(x)>=y)
+            double y = mm * rnd.nextDouble();
+            double x = start + (end - start) * rnd.nextDouble();
+            if (f.eval(x) >= y) {
                 cont++;
+            }
         }
-        return (double)cont/times * (end-start)*mm;
-        
-    
+        return (double) cont / times * (end - start) * mm;
+
     }
 }
